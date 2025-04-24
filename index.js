59d3f3b6cb21f4bc9134bda9f6061c9fc94623a1
Shwetha <Shwetha.Venkatesh@itorizon.com>
2025-04-25 01:14:12 +0530

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById("book_now");
+function load_table(){
+    
+}
+function style_change(){
+    btn.style.transform="scale(0.8,0.8)";
+}
+function style_change1(){
+    btn.style.transform="scale(1,1)";
+}
